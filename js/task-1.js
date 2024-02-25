d8b4ee62c0368d9e9fc4124a890a53d55936f0a6
Yelyzaveta2004 <ucomfy50@gmail.com>
2024-02-25 12:02:17 +0200

    розмітка+таск1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,8 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const categories = document.querySelectorAll('ul#categories > .item');
+console.log(`У списку ${categories.length} категорії.`);
+categories.forEach((category) => {
+  const title = category.querySelector('h2').textContent;
+  const items = category.querySelectorAll('li');
+  console.log(`Категорія: ${title}`);
+  console.log(`Кількість елементів: ${items.length}`);
+});
